6be384300ce6cf1e860b72021562ee27542af507
Stéphan Barbé <sbarbe@Stephans-MacBook-Pro.local>
2015-10-02 16:06:12 +0200

    custom app/index.js

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,29 +2,16 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var mkdirp = require('mkdirp');
 
 module.exports = yeoman.generators.Base.extend({
   prompting: function () {
-    var done = this.async();
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the flawless ' + chalk.red('Hydraweb') + ' generator!'
+      'Welcome to the ' + chalk.red('HydraWeb') + ' generator!'
     ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
-
-      done();
-    }.bind(this));
   },
 
   writing: {
@@ -48,6 +35,18 @@ module.exports = yeoman.generators.Base.extend({
         this.templatePath('jshintrc'),
         this.destinationPath('.jshintrc')
       );
+    },
+
+    misc: function () {
+      mkdirp('code/src/styles');
+      mkdirp('code/src/scripts');
+      mkdirp('code/src/templates');
+      mkdirp('code/dist/images');
+      mkdirp('code/dist/fonts');
+      mkdirp('code/dist/js');
+      mkdirp('code/dist/js/libs');
+      mkdirp('code/dist/lang');
+      mkdirp('code/dist/media');
     }
   },
 
